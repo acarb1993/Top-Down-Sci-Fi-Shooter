986a2ace2c1a2c322d9a8a7f68d4ce797bf6dacb
Adam Carballido <carballido.adam@yahoo.com>
2020-05-01 22:32:32 -0400

    Particle cleanup on Projectiles

--- Top Down Game/Assets/Scripts/Weapon Scripts/Projectile.cs
+++ Top Down Game/Assets/Scripts/Weapon Scripts/Projectile.cs
@@ -9,7 +9,7 @@ public class Projectile : MonoBehaviour
     protected ProjectileStats projectileStats;
 
     [SerializeField]
-    protected Transform impact;
+    protected GameObject impact;
 
     public ProjectileStats BulletStats { get { return projectileStats; } }
 
@@ -42,7 +42,8 @@ public class Projectile : MonoBehaviour
             Despawn();
         }
 
-        Instantiate(impact, transform.position, transform.rotation);
+        GameObject particle = Instantiate(impact, transform.position, transform.rotation);
+        Destroy(particle, 3);
     }
 
     public void SpawnProjectile(Transform spawnPoint)
